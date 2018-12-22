0cb494ac0e4db7343ff37f5bb3fa2450ead16153
Your NameSagar <samtani.sagar@gmail.com>
2018-12-22 19:41:33 +0530

    Updates

---
+++ precache-manifest.fc1e7830f7ba1d2c5975ef993b717c2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3140491bdaf1234b6c3d",
+    "url": "/big-tree/static/js/runtime~main.3140491b.js"
+  },
+  {
+    "revision": "13a8b960d1def4fdf760",
+    "url": "/big-tree/static/js/main.13a8b960.chunk.js"
+  },
+  {
+    "revision": "e130c10b169b6631f3da",
+    "url": "/big-tree/static/js/1.e130c10b.chunk.js"
+  },
+  {
+    "revision": "13a8b960d1def4fdf760",
+    "url": "/big-tree/static/css/main.84ddf6b2.chunk.css"
+  },
+  {
+    "revision": "b3d4f8cf9f0dd679d3b46116b751b530",
+    "url": "/big-tree/index.html"
+  }
+];
